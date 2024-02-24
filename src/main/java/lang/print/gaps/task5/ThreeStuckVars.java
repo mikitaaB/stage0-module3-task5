fe5d68c1a4f95d3346027d6153ece37f71afe9c8
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-25 01:32:00 +0300

    module 3. task 5

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 10;
+        int c = 100;
+        System.out.println(a+""+b+""+c);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float secondsRes = minutes * 60;
+        System.out.println(secondsRes);
     }
 }
