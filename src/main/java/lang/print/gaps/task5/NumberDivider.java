23abd8826fd42a9290068f21e2f52a59a4e13a19
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-04 01:25:10 +0300

    module 3. task 5. fix

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int res = toBeDivided / 5;
+        double res = toBeDivided / 5;
         System.out.println(res);
     }
 }
